2519dbe0db3dac67bfe68fe833b126689462f502
Nesbittl5470 <47502024+Nesbittl5470@users.noreply.github.com>
2019-02-10 11:20:44 -0500

    Add files via upload
    First Submission

---
+++ P2T1_SalesPrediction_LegranttNesbitt.py
@@ -0,0 +1,18 @@
+print('This is a Sales Prediction Program')
+print('Feb 9 2019')
+print('CTI-110 P2T1 - Sales Prediction')
+print('Legrantt Nesbitt')
+
+# Ask user what is projected amount of total sales
+
+total_sales = int(input(' What is the projected amout of total sales?: '))
+
+percentage = 0.23
+
+# Projected amont of total sales * percent
+
+print ('Total profit sales', total_sales * percentage )
+
+                   
+
+
