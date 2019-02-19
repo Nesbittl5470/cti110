6e546a8ddb9b551bd0b92d2ed7df6797c3697410
Nesbittl5470 <47502024+Nesbittl5470@users.noreply.github.com>
2019-02-19 18:22:28 -0500

    Add files via upload

---
+++ P2HW1_PoundsKilograms_LegranttNesbitt.py
@@ -0,0 +1,18 @@
+print('This is a Simple Pounds to Kilograms Converter')
+print('Feb 10 2019')
+print('CTI-110 Tutorial 1 - Pounds to Kilograms Converter ')
+print('Legrantt Nesbitt')
+
+
+
+# Ask User how many pounds are there
+
+pounds = int(input('How many Pounds are there?: '))
+
+byKilograms = pounds / 2.2046
+
+# Calculate/Display Pounds by in Kilograms
+
+print ('This is the amount of Kilograms', \
+       format(byKilograms, '.2f') )
+
